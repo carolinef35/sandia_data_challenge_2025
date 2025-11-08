7e49457a14672293f17f8ffcf05be94bbf8f6f38
carolinef35 <cf35@illinois.edu>
2025-11-08 14:42:42 -0600

    cleaned virgin dataset

--- vrigin.py
+++ vrigin.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("data/AllData_PreEDM_Virgin_RowColIDs.csv")
+df = pd.read_csv("data/AllData_PreEDM_Virgin_RowColIDs_correcteddates.csv")
 
 # Convert BuildDate column to date time
 df["BuildDate"] = pd.to_datetime(df["BuildDate"], errors="coerce")
@@ -10,17 +10,17 @@ df = df[df["Powder"].str.lower().str.contains("virgin")]
 
 # Define valid builds
 valid_builds = {
-    "A": {"date": "2019-08-05", "layout": "6x6"}, 
-    "B": {"date": "2019-08-06", "layout": "6x6TA"},
-    "C": {"date": "2019-08-07", "layout": "11x11TA"}, 
-    "D": {"date": "2019-08-08", "layout": "11x11TA"},
-    "E": {"date": "2019-08-09", "layout": "6x6"},
-    "F": {"date": "2019-08-12", "layout": "6x6TA"}, 
-    "G": {"date": "2019-08-13", "layout": "6x6TA"},
-    "H": {"date": "2019-08-14", "layout": "11x11TA"}, 
-    "I": {"date": "2019-08-15", "layout": "6x6"},
-    "J": {"date": "2019-08-16", "layout": "11x11TA"},
-    "K": {"date": "2019-08-28", "layout": "11x11TA"},
+    "A": {"date": "2019-08-05", "layout": "6X6"}, 
+    "B": {"date": "2019-08-06", "layout": "6X6TA"},
+    "C": {"date": "2019-08-07", "layout": "11X11TA"}, 
+    "D": {"date": "2019-08-08", "layout": "11X11TA"},
+    "E": {"date": "2019-08-09", "layout": "6X6"},
+    "F": {"date": "2019-08-12", "layout": "6X6TA"}, 
+    "G": {"date": "2019-08-13", "layout": "6X6TA"},
+    "H": {"date": "2019-08-14", "layout": "11X11TA"}, 
+    "I": {"date": "2019-08-15", "layout": "6X6"},
+    "J": {"date": "2019-08-16", "layout": "11X11TA"},
+    "K": {"date": "2019-08-28", "layout": "11X11TA"},
 }
 
 keep_rows = []
@@ -28,17 +28,21 @@ for i, row in df.iterrows():
     plate = str(row["PlateID"]).strip()
     layout = str(row["Layout"]).strip().upper()
     build_date = row["BuildDate"].strftime("%Y-%m-%d") if not pd.isna(row["BuildDate"]) else None
+    # print(plate, layout, build_date)
 
     if plate in valid_builds:
         valid_date = valid_builds[plate]["date"]
         valid_layout = valid_builds[plate]["layout"]
+        # print(valid_date, valid_layout)
         if layout == valid_layout and build_date == valid_date:
             keep_rows.append(True)
         else:
             keep_rows.append(False)
     else:
         keep_rows.append(False)
-print(keep_rows)
-# df_clean = df[keep_rows].copy()
-# summary = df_clean.groupby(["PlateID", "Layout", "BuildDate"]).size().reset_index(name="Count")
-# print(summary)
+
+# print(keep_rows)
+df_clean = df[keep_rows].copy()
+summary = df_clean.groupby(["PlateID", "Layout", "BuildDate"]).size().reset_index(name="Count")
+print(summary)
+
